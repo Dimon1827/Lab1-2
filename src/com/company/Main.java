55e5c42f493d73056d9833fb6682ff7a97e861c5
Dimon1827 <105965169+Dimon1827@users.noreply.github.com>
2022-05-21 19:08:22 +0300

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Tasks.class);
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("введите 1 для режима работы ввода параметров задач" +
+                " с клавиатуры или 2 для ввода параметров с файла");
+        Scanner scanner = new Scanner(System.in);
+        int workingMode = scanner.nextInt();
+        if (workingMode== 1 || workingMode== 2) {
+            Tasks tasks = new Tasks();
+            tasks.task1(workingMode);
+            log.debug("1 задание выполнено");
+            tasks.task2(workingMode);
+            log.debug("2 задание выполнено");
+            tasks.task3(workingMode);
+            log.debug("3 задание выполнено");
+            tasks.task4(workingMode);
+            log.debug("4 задание выполнено");
+            tasks.task5(workingMode);
+            log.debug("5 задание выполнено");
+            tasks.task6(workingMode);
+            log.debug("6 задание выполнено");
+            tasks.task7(workingMode);
+            log.debug("7 задание выполнено");
+            tasks.task8(workingMode);
+            log.debug("8 задание выполнено");
+
+
+
+        }
+        else {
+            System.out.println("Нет такого режима работы программы");
+        }
+    }
+
+
+}
